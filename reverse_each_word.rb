178443573dabd36f16335ede52daa51acf1a306d
Ellaine Tolentino <rckrleyn@gmail.com>
2020-09-23 22:20:22 +0000

    Automatically backed up by Learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,16 @@
+
+
+string = "Hello there, and how are you?"
+hello = "Ako jejemon"
+
+def reverse_each_word(string)
+i = 0
+
+  if i < string.length 
+  new_array = string.split
+  new_array[i].reverse
+end
+i += 1
+end
+
+
