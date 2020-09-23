f7b9ff2b0a8a7453cca5f39a41cca5636b1294e8
Ellaine Tolentino <rckrleyn@gmail.com>
2020-09-23 22:51:12 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,14 +2,24 @@
 
  
 string = "Hello there, and how are you?"
-# hello = "Ako jejemon, Ikaw sino ka?"
+hello = "Ako jejemon, Ikaw sino ka?"
 
-def reverse_each_word(string)
+# def reverse_each_word(string)
+#   i = 0
+#   new_array = string.split
+#     if i < new_array.length 
+#     new_array[i].reverse
+#     i += 1
+#   end
+#   new_array
+# end
+
+def test_this(string)
   string = string.split
-  string.collect {|i| i.reverse}
-    
+  string.each do |i| 
+    puts i.reverse
   end
-
+  string
 end
- reverse_each_word(string)
-# test_this(hello)
+ 
+test_this(hello)
